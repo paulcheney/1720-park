b0c5f91d3e6e65cad0c823688dad1a621e8502bd
Paul Cheney <spartandesign.biz@gmail.com>
2025-11-07 16:40:30 -0700

    first

---
+++ data/parks.mjs
@@ -0,0 +1,63 @@
+const parks = [
+    {
+      "name": "Kakadu National Park",
+      "description": "Vast Top End landscapes of wetlands, floodplains and escarpments with internationally significant Aboriginal rock art and wildlife.",
+      "address": "Kakadu Highway, Jabiru NT 0886, Australia",
+      "size_sq_mi": 7645.6,
+      "established": "April 5, 1979",
+      "url": "https://parksaustralia.gov.au/kakadu/",
+      "photo_url": "kakadu.webp",
+      "rating": 5
+    },
+    {
+      "name": "Uluru-Kata Tjuta National Park",
+      "description": "Sacred Aṉangu land featuring Uluru and the domed Kata Tjuta formations in Australia’s Red Centre; jointly managed with Traditional Owners.",
+      "address": "Lasseter Highway, Uluru NT 0872, Australia",
+      "size_sq_mi": 511.9,
+      "established": "1958",
+      "url": "https://uluru.gov.au/",
+      "photo_url": "uluru-kata.webp",
+      "rating": 5
+    },
+    {
+      "name": "Booderee National Park",
+      "description": "White-sand beaches, coastal heath and forests on Jervis Bay; co-managed with the Wreck Bay Aboriginal Community and home to a botanic garden.",
+      "address": "Jervis Bay Territory 2540, Australia",
+      "size_sq_mi": 24.6,
+      "established": "1992",
+      "url": "https://parksaustralia.gov.au/booderee",
+      "photo_url": "booderee.webp",
+      "rating": 5
+    },
+    {
+      "name": "Christmas Island National Park",
+      "description": "Protects most of Christmas Island's rainforest and coastline; famous for the annual red crab migration and rich marine life.",
+      "address": "Christmas Island, Indian Ocean Territory, Australia",
+      "size_sq_mi": 32.8,
+      "established": "February 21, 1980",
+      "url": "https://parksaustralia.gov.au/christmas",
+      "photo_url": "christmas.webp",
+      "rating": 5
+    },
+    {
+      "name": "Pulu Keeling National Park",
+      "description": "Remote coral atoll (North Keeling Island) in the Cocos (Keeling) Islands, protecting seabird colonies, turtles and pristine reefs.",
+      "address": "North Keeling Island, Cocos (Keeling) Islands, Australia",
+      "size_sq_mi": 10.0,
+      "established": "December 12, 1995",
+      "url": "https://parksaustralia.gov.au/pulu-keeling",
+      "photo_url": "pulu.webp",
+      "rating": 4
+    },
+    {
+      "name": "Norfolk Island National Park and Botanic Garden",
+      "description": "Subtropical rainforest and endemic species (including the Norfolk Island pine) across Mount Pitt, Mount Bates and adjacent reserves.",
+      "address": "Norfolk Island 2899, Australia",
+      "size_sq_mi": 2.5,
+      "established": "1986",
+      "url": "https://norfolkislandnationalpark.gov.au",
+      "photo_url": "norfolk.webp",
+      "rating": 5
+    }
+  ]
+  
